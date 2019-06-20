6a7c2809f2b450f2170b31394671c4dd811585ee
Achint Satsangi <achint.satsangi@gmail.com>
2019-06-20 13:53:57 +0200

    Added react components for file upload.. - Supports one file at a time - Added basic frontend validations for file type - Not handling the backend response yet

--- document-redactor-frontend/src/App.js
+++ document-redactor-frontend/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Uploader from "./components/uploader";
+import FileObject from "./components/fileObject";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    };
+  }
+
+  onClickHandler = () => {
+    const data = new FormData();
+    data.append("file", this.state.selectedFile);
+    axios
+      .post("/upload", data, {
+        // receive two    parameter endpoint url ,form data
+      })
+      .then(res => {
+        // then print response status
+        console.log(res.statusText);
+      });
+  };
+
+  onChangeHandler = event => {
+    console.log(event.target.files);
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0
+    });
+  };
+
+  isValidFileSelected() {
+    return this.state.selectedFile !== null
+      ? this.state.selectedFile.type.startsWith("image")
+      : false;
+  }
+
+  render() {
+    console.log(this.state.selectedFile);
+    console.log(this.isValidFileSelected());
+    return (
+      <React.Fragment>
+        <NavBar />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 center">
+              <Uploader
+                onFileChange={this.onChangeHandler}
+                onClickUpload={this.onClickHandler}
+                isUploadAllowed={this.isValidFileSelected()}
+              />
+              <FileObject obj={this.state.selectedFile} />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ document-redactor-frontend/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Stateless functional component
+function NavBar() {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Document Redactor
+      </a>
+    </nav>
+  );
+}
+
+export default NavBar;
