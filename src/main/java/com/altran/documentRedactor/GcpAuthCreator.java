9c69148d90d1e2a2fddc24cb42ba6915433b59bf
Achint Satsangi <achint.satsangi@gmail.com>
2019-06-24 09:59:26 +0200

    Remove logging GCP credentials

--- src/main/java/com/altran/documentRedactor/GcpAuthCreator.java
+++ src/main/java/com/altran/documentRedactor/GcpAuthCreator.java
@@ -8,9 +8,6 @@ import java.nio.file.StandardOpenOption;
 public class GcpAuthCreator {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(System.getenv("GCP_KEY_FILE"));
-        System.out.println(System.getenv("GCP_CRED"));
-        System.out.println(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
         Files.createFile(Path.of(System.getenv("GCP_KEY_FILE")));
         Files.writeString(Path.of(System.getenv("GCP_KEY_FILE")), System.getenv("GCP_CRED"), StandardOpenOption.WRITE);
     }
