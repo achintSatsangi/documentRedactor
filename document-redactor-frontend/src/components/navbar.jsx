47c8d12eed8e29d889853025d0f77420f50d8b91
Achint Satsangi <achint.satsangi@gmail.com>
2019-06-25 14:14:47 +0200

    Some fun... created logo and favico icon for browser loads

--- document-redactor-frontend/src/components/navbar.jsx
+++ document-redactor-frontend/src/components/navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Logo from "../dr.png";
 
 //Stateless functional component
 function NavBar() {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
+        <img src={Logo} height="50em" width="50em" />
         Document Redactor
       </a>
     </nav>
